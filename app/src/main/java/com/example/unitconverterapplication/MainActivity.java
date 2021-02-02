322f46700b98318f2e7bbf5290b66f5d9f5d4643
John Rey <42550367+SirPancakes@users.noreply.github.com>
2021-02-01 21:55:27 -0800

    updated Feb. 1, 2021 - 9:55pm

--- app/src/main/java/com/example/unitconverterapplication/MainActivity.java
+++ app/src/main/java/com/example/unitconverterapplication/MainActivity.java
@@ -45,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
         else if (spinnerTemp1.getSelectedItem().equals("Fahrenheit") && spinnerTemp2.getSelectedItem().equals("Celsius")) {
             double fahrenheit = Double.parseDouble(valueEntered1);
             double celsius = fahrenheit - 32 * 5/9;
-            tempNum2.setText("" + celsius);
-
+            String answer = String.format("%.2f", String.valueOf(celsius));
+            tempNum2.setText("" + answer);
 
             Toast.makeText(this, "Converted", Toast.LENGTH_SHORT).show();
         }
@@ -54,62 +54,4 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Both can't be the same unit! Please change the other unit", Toast.LENGTH_SHORT).show();
         }
     }
-
-
-
-
-
-
-
-/*        if (spinnerTemp1.getSelectedItem().equals("Celsius") && spinnerTemp2.getSelectedItem().equals("Fahrenheit")) {
-            double celsius = Double.parseDouble(valueEntered1);
-            double fahrenheit = (celsius * (9/5)) + 32;
-            tempNum2.setText("" + fahrenheit + "°");
-
-
-            Toast.makeText(this, "Converted", Toast.LENGTH_SHORT).show();
-        }
-
-        else if (spinnerTemp1.getSelectedItem().equals("Fahrenheit") && spinnerTemp2.getSelectedItem().equals("Celsius")) {
-            double fahrenheit = Double.parseDouble(valueEntered1);
-            double celsius = fahrenheit - 32 * 5/9;
-            tempNum2.setText("" + celsius + "°");
-
-
-            Toast.makeText(this, "Converted", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            Toast.makeText(this, "Both can't be the same unit! Please change the other unit", Toast.LENGTH_SHORT).show();
-        }
-
-        */
-
-
-
-
-
-
-/*
-            spinnerTemp2.equals("Fahrenheit");
-
-            if (spinnerTemp2.equals("Fahrenheit")) {
-                /*
-            }
-
-        } else if (spinnerTemp1.equals("Fahrenheit")) {
-            double fahrenheit = Double.parseDouble(valueEntered1);
-            spinnerTemp2.equals("Celsius");
-
-            if (spinnerTemp2.equals("Celsius")) {
-                double celsius = (fahrenheit - 32) * (5 / 9);
-                tempNum2.setText("" + celsius + "°");
-            }
-        }
-*/
-
-    /*public void convert() {
-        String valueEntered = tempNum1.getText().toString();
-    }*/
-
-
 }
